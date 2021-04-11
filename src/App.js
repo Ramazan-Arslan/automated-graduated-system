e2a7d84d5e9f31f6b2af37fb2a873eafeffd3d59
Ramazan Arslan <ramazan@decktopus.com>
2021-04-12 02:45:36 +0300

    creating template and some practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import logo from './assets/icons/logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import ResponsiveDrawer from './components/sidebar.component/siderbar.component.js';
 
 function App() {
   return (
@@ -7,17 +9,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Guyss Bro !
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+         
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary">
+          Hello World
+          </Button>
+          <ResponsiveDrawer/>
       </header>
+         
     </div>
   );
 }
