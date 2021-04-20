b9162715d4b1feb83fb24820dcc98849624e6110
Bekir Yörük <bekiryoruk1@gmail.com>
2021-04-20 06:05:14 +0300

    adding header linking

--- src/components/header.component/header.component.js
+++ src/components/header.component/header.component.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import './header.component.css';
-
+import React from 'react'
+import './header.component.css'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
-import { Avatar } from '@material-ui/core';
+import { Avatar } from '@material-ui/core'
 
-export default function Header(){
-    return(
-      <div className="header">
-          <div className="headericon">
-            <FontAwesomeIcon style={{ height:'100px'}} icon={faHome} />
+export default function Header() {
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none',
+  }
+  return (
+    <div className='header'>
+      <Router>
+        <div className='headericon'>
+          <Link style={linkStyle} to='/homepage'>
+            <FontAwesomeIcon style={{ height: '100px' }} icon={faHome} />
+          </Link>
+          <Link style={linkStyle} to='/notification'>
             <FontAwesomeIcon icon={faBell} />
-            </div>
-
-            <div className="userCard">
-                <Avatar alt="Remy Sharp" src="/broken-image.jpg" className="userAvatar">F</Avatar>
-                <div className="userInfo">
-                  <p className="userTitle">Furkan Sahin</p> {/* İlerde burası databaseden çekilcek user.name gibi*/}
-                  <p className="userType">Student</p>
-                </div>
-            </div>
-
-
+          </Link>
         </div>
+      </Router>
 
-
-
- )         
-};
+      <div className='userCard'>
+        <Avatar alt='Remy Sharp' src='/broken-image.jpg' className='userAvatar'>
+          F
+        </Avatar>
+        <div className='userInfo'>
+          <p className='userTitle'>Furkan Sahin</p>{' '}
+          {/* İlerde burası databaseden çekilcek user.name gibi*/}
+          <p className='userType'>Student</p>
+        </div>
+      </div>
+    </div>
+  )
+}
