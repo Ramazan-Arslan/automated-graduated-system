de498c109ccae2785cbc8a01e3e8d74602f6a759
Bekir Yörük <bekiryoruk1@gmail.com>
2021-05-04 21:39:54 +0300

    student approval small fixes

--- src/components/student-approval.component/student-approval.component.js
+++ src/components/student-approval.component/student-approval.component.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import {Prompt} from 'react-router-dom'
+import { Prompt } from 'react-router-dom'
 import './student-approval.component.css'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import '../approval-request-card.component/approval-request-card.component.css';
+import '../approval-request-card.component/approval-request-card.component.css'
 import { Avatar, Button } from '@material-ui/core'
 import Modal from '@material-ui/core/Modal'
-import Helper from './student-approval-helper';
-
-
+import Helper from './student-approval-helper'
 
 async function submitProposalDecisions(advisorId, acceptedList, rejectedList) {
-  const result = await Helper.submitProposalDecisions(advisorId, acceptedList, rejectedList)
-  if (result === "Succesfull") {
-    alert("Succesfull.")
+  const result = await Helper.submitProposalDecisions(
+    advisorId,
+    acceptedList,
+    rejectedList
+  )
+  if (result === 'Succesfull') {
+    alert('Saved succesfully.')
     window.location.reload(true)
-  }
-  else {
-    alert("Some data are not set.")
+  } else {
+    alert('Some data are not set.')
   }
 }
 
-
 export default function StudentApproval() {
-
   var userId = localStorage.getItem('id')
   var userType = localStorage.getItem('type')
   const [studentList, setStudentList] = useState([])
@@ -33,11 +32,10 @@ export default function StudentApproval() {
   const [modalIsOpen, setOpenModal] = useState(false)
 
   useEffect(async () => {
-    var list = await Helper.getStudentsInfo(userId);
-    setStudentList(list);
-    var data = await Helper.getPreviewData(userId, acceptedList, rejectedList);
-    setPreviewData(data);
-
+    var list = await Helper.getStudentsInfo(userId)
+    setStudentList(list)
+    var data = await Helper.getPreviewData(userId, acceptedList, rejectedList)
+    setPreviewData(data)
   }, [])
 
   function removeStudentCard(student, list, setList) {
@@ -45,56 +43,96 @@ export default function StudentApproval() {
     setList(updatedList)
   }
 
-
-
   function getItemView(student, text) {
     return (
       <div className='approval-request'>
         <div>
-          <Avatar alt='Remy Sharp' src='/broken-image.jpg' className='userAvatar'>
+          <Avatar
+            alt='Remy Sharp'
+            src='/broken-image.jpg'
+            className='userAvatar'
+          >
             {student.name.charAt(0)}
           </Avatar>
         </div>
         <div className='requester-info'>
-          <p className='requester-name'>{student.name + " " + student.surname}</p>
+          <p className='requester-name'>
+            {student.name + ' ' + student.surname}
+          </p>
           <p className='requester-department'>{student.department}</p>
         </div>
-        {text === "Proposals" && <div className='decide-buttons'>
-          <Button className='decide accept'
-            onClick={() => { acceptedList.push(student); var method = setStudentList; removeStudentCard(student, studentList, method) }} >Accept</Button>
+        {text === 'Proposals' && (
+          <div className='decide-buttons'>
+            <Button
+              className='decide accept'
+              onClick={() => {
+                acceptedList.push(student)
+                var method = setStudentList
+                removeStudentCard(student, studentList, method)
+              }}
+            >
+              Accept
+            </Button>
 
-          <Button className='decide reject'
-            onClick={() => { rejectedList.push(student); var method = setStudentList; removeStudentCard(student, studentList, method) }} >Reject</Button>
-        </div>}
+            <Button
+              className='decide reject'
+              onClick={() => {
+                rejectedList.push(student)
+                var method = setStudentList
+                removeStudentCard(student, studentList, method)
+              }}
+            >
+              Reject
+            </Button>
+          </div>
+        )}
 
-        {text === "Accepted Proposals" && <div className='decide-buttons'>
-          <Button className='decide accept'
-            onClick={() => { studentList.push(student); var method = setAcceptedList; removeStudentCard(student, acceptedList, method) }} >Undo</Button>
-        </div>}
+        {text === 'Accepted Proposals' && (
+          <div className='decide-buttons'>
+            <Button
+              className='decide accept'
+              onClick={() => {
+                studentList.push(student)
+                var method = setAcceptedList
+                removeStudentCard(student, acceptedList, method)
+              }}
+            >
+              Undo
+            </Button>
+          </div>
+        )}
 
-        {text === "Rejected Proposals" && <div className='decide-buttons'>
-          <Button className='decide accept'
-            onClick={() => { studentList.push(student); var method = setRejectedList; removeStudentCard(student, rejectedList, method) }} >Undo</Button>
-        </div>}
+        {text === 'Rejected Proposals' && (
+          <div className='decide-buttons'>
+            <Button
+              className='decide accept'
+              onClick={() => {
+                studentList.push(student)
+                var method = setRejectedList
+                removeStudentCard(student, rejectedList, method)
+              }}
+            >
+              Undo
+            </Button>
+          </div>
+        )}
       </div>
-    );
+    )
   }
 
   function getListView(text, list) {
     return (
       <div>
         <h1>{text}</h1>
-        <List style={{
-          display: 'flex',
-          flexDirection: 'column',
-          padding: 0,
-        }}>
+        <List
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: 0,
+          }}
+        >
           {list.map((tile) => (
-
-            <ListItem
-              key={tile.name}
-              cols={tile.cols || 1}>
-
+            <ListItem key={tile.name} cols={tile.cols || 1}>
               {getItemView(tile, text)}
             </ListItem>
           ))}
@@ -103,32 +141,35 @@ export default function StudentApproval() {
     )
   }
 
-
-
   function getPreviewView(list) {
-
     return (
       <div>
-        <List style={{
-          display: 'flex',
-          flexDirection: 'column',
-          padding: 0,
-        }}>
+        <List
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: 0,
+          }}
+        >
           {list.map((tile) => (
-
-            <ListItem
-              key={tile.student.id}
-              cols={tile.cols || 1}>
-
+            <ListItem key={tile.student.id} cols={tile.cols || 1}>
               <div className='approval-request'>
                 <div>
-                  <Avatar alt='Remy Sharp' src='/broken-image.jpg' className='userAvatar'>
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='/broken-image.jpg'
+                    className='userAvatar'
+                  >
                     {tile.student.name.charAt(0)}
                   </Avatar>
                 </div>
                 <div className='requester-info'>
-                  <p className='requester-name'>{tile.student.name + " " + tile.student.surname}</p>
-                  <p className='requester-department'>{tile.student.department}</p>
+                  <p className='requester-name'>
+                    {tile.student.name + ' ' + tile.student.surname}
+                  </p>
+                  <p className='requester-department'>
+                    {tile.student.department}
+                  </p>
                 </div>
                 <h1>{tile.status}</h1>
               </div>
@@ -147,18 +188,30 @@ export default function StudentApproval() {
     )
   }
 
-
   return (
     <div className='student-approval'>
-      {getListView("Proposals", studentList)}
-      {getListView("Accepted Proposals", acceptedList)}
-      {getListView("Rejected Proposals", rejectedList)}
+      {getListView('Proposals', studentList)}
+      {getListView('Accepted Proposals', acceptedList)}
+      {getListView('Rejected Proposals', rejectedList)}
 
-      <Button className='decide accept' disabled={!Boolean(acceptedList[0]) && !Boolean(rejectedList[0])}
-        onClick={() => { submitProposalDecisions(userId, acceptedList, rejectedList) }} >Save</Button>
+      <Button
+        className='decide accept'
+        disabled={!Boolean(acceptedList[0]) && !Boolean(rejectedList[0])}
+        onClick={() => {
+          submitProposalDecisions(userId, acceptedList, rejectedList)
+        }}
+      >
+        Save Changes
+      </Button>
 
-      <Button className='decide accept'
-        onClick={() => { setOpenModal(true) }} >Preview</Button>
+      <Button
+        className='decide accept'
+        onClick={() => {
+          setOpenModal(true)
+        }}
+      >
+        Preview
+      </Button>
       <Modal
         open={modalIsOpen}
         onClose={!modalIsOpen}
@@ -166,12 +219,11 @@ export default function StudentApproval() {
         aria-describedby='simple-modal-description'
       >
         {getPreviewView(previewData)}
-
       </Modal>
 
       <Prompt
         when={Boolean(acceptedList[0]) || Boolean(rejectedList[0])}
-        message="Changes are not saved. Want to leave?"
+        message='Changes are not saved. Want to leave?'
       />
     </div>
   )
