46ab25fa6471e4ae20ab2ce7d4b765e0d335f19c
Ramazan Arslan <ramazan@decktopus.com>
2021-05-22 20:39:36 +0300

    https to http

--- src/server_connections/AGS/getHostAddressAGS.js
+++ src/server_connections/AGS/getHostAddressAGS.js
@@ -1,4 +1,4 @@
 const getHostAddressAGS = () => {
-    return 'https://ec2-18-223-0-35.us-east-2.compute.amazonaws.com:22';
+    return 'http://ec2-18-223-0-35.us-east-2.compute.amazonaws.com:22';
 }
 export default getHostAddressAGS;
--- src/server_connections/OBS/getHostAddressOBS.js
+++ src/server_connections/OBS/getHostAddressOBS.js
@@ -1,4 +1,4 @@
 const getHostAddressOBS = () => {
-    return 'https://ec2-18-223-0-35.us-east-2.compute.amazonaws.com:8000';
+    return 'http://ec2-18-223-0-35.us-east-2.compute.amazonaws.com:8000';
 }
 export default getHostAddressOBS;
